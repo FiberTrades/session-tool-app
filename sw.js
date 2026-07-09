2dfa6b174d55ff71fdc8ff67d6f9475b9d0b4a8e
FiberTrades <fibertrades121@gmail.com>
2026-07-09 20:50:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // skipWaiting + clients.claim mean a new version takes over immediately instead of
 // waiting for every tab to close.
 
-const CACHE = 'session-tool-v7';
+const CACHE = 'session-tool-v8';
 const CORE = ['./', './index.html'];
 
 self.addEventListener('install', (event) => {
