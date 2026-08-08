d979be008e6f2da6e1e4c8a8a5bbd69f1d102c00
FiberTrades <fibertrades121@gmail.com>
2026-08-08 17:06:36 +0100

    Replay: move the 'more chart' toggle further left to clear the price axis
    Build 16:06; sw n313

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * forget, network-first means the page itself still updates on the next load.
  */
 
-const CACHE_VERSION = 'st-2026-08-08n312';    // <-- bump this string on each deploy
+const CACHE_VERSION = 'st-2026-08-08n313';    // <-- bump this string on each deploy
 const APP_CACHE     = CACHE_VERSION + '-app';
 const RUNTIME_CACHE = CACHE_VERSION + '-rt';
 
